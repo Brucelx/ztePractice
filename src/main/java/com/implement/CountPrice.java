3f7a89101362e76226f047de24d69aa38e3a2f24
LiuXiao <liuxiao@pachiratech.com>
2018-01-16 18:41:15 +0800

    添加计算书价的实现类

---
+++ src/main/java/com/implement/CountPrice.java
@@ -0,0 +1,24 @@
+package com.implement;
+
+import com.entity.Book;
+
+import java.util.Scanner;
+
+/**
+ * Created by Lenovo on 2018/1/16.
+ */
+public class CountPrice {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Book book;
+        while (scanner.hasNext()){
+            String name = scanner.next();
+            float price = scanner.nextFloat();
+            float discount = scanner.nextFloat();
+            book = new Book(name, price, discount);
+            float sell = price * discount;
+            System.out.println("书名：" + name + "出售价格:" + sell);
+
+        }
+    }
+}
---
+++ src/test/java/Unittest.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Lenovo on 2018/1/16.
+ */
+public class Unittest {
+}
