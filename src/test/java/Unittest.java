a0e36897e020b9980cd5e3314db67f0b703b2267
LiuXiao <liuxiao@pachiratech.com>
2018-01-16 18:52:23 +0800

    添加单元测试

--- src/test/java/Unittest.java
+++ src/test/java/Unittest.java
@@ -1,7 +1,18 @@
-package PACKAGE_NAME;
+import com.implement.CountPrice;
+import org.junit.Test;
 
 /**
  * Created by Lenovo on 2018/1/16.
  */
 public class Unittest {
+    @Test
+    public void testPrice(){
+        CountPrice countPrice = new CountPrice();
+        String name = "西游记";
+        float price = 98.0f;
+        float discount = (float)0.8;
+
+        countPrice.countPrice(name, price, discount);
+
+    }
 }
